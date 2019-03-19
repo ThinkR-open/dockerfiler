ab0af6bc597a371b59c8829a0a614760ef7ea86b
Colin <colin@thinkr.fr>
2019-03-19 16:17:20 +0100

    updated the dock_from_description function

--- R/dock_from_desc.R
+++ R/dock_from_desc.R
@@ -2,28 +2,74 @@
 #'
 #' This function will parse a DESCRIPTION file and
 #' create a Dockerfile that remotes::install_cran
-#' all the functions from the Imports section.
+#' all the functions from the Imports section, add
+#' the COPY of the tar.gz of the package, and install
+#' the package.
 #'
 #' @param path Path to DESCRIPTION
 #' @param FROM FROM of the Dockerfile
 #' @param AS AS of the Dockerfile
 #'
-#' @return
+#' @note After this Dockerfile is created, the package
+#'     should be built and be put in the same directory
+#'     as the dockerfile.
+#'
+#' @return A Dockerfile Object.
 #' @export
 #'
+#' @importFrom utils installed.packages
+#'
 #' @examples
-dock_from_desc <- function(path = "DESCRIPTION", FROM = "rocker/r-base", AS = NULL){
+#' \dontrun{
+#' my_dock <- dock_from_desc("DESCRIPTION")
+#' my_dock
+#' my_dock$CMD(r(library(dockerfiler)))
+#' my_dock$add_after(
+#' cmd = "RUN R -e 'remotes::install_cran(\"rlang\")'",
+#' after = 3
+#' )
+#' }
+dock_from_desc <- function(
+  path = "DESCRIPTION",
+  FROM = "rocker/r-base",
+  AS = NULL
+){
 
   x <- Dockerfile$new(FROM, AS)
-  x$RUN(r(install.packages('remotes')))
+  x$RUN("R -e 'install.packages(\"remotes\")'")
+
+  # We need to be sure install_cran is there
+  x$RUN("R -e 'remotes::install_github(\"r-lib/remotes\", ref = \"6c8fdaa\")'")
 
   desc <- read.dcf(path)
-  desc <- desc[, "Imports"]
-  desc <- gsub(",", "", desc)
-  desc <- strsplit(desc, "\n")[[1]]
-  for (i in seq_along(desc)){
-    x$RUN(paste0("R -e 'remotes::install_cran(\"", desc[i], "\")'"))
+
+  # Handle cases where there is no deps
+  imp <- attempt::attempt({
+    desc[, "Imports"]
+  }, silent = TRUE)
+
+  if (class(r)[1] != "try-error"){
+    # Remove base packages which are not on CRAN
+    # And shouldn't be installed
+    reco <- rownames(installed.packages(priority="base"))
+
+    if (length(imp) > 0) {
+
+      imp <- gsub(",", "", imp)
+      imp <- strsplit(imp, "\n")[[1]]
+      for (i in seq_along(imp)){
+        if (!(imp[i] %in% reco)){
+          x$RUN(paste0("R -e 'remotes::install_cran(\"", imp[i], "\")'"))
+        }
+      }
+    }
   }
+
+  x$COPY(
+    from = paste0(desc[1], "_*.tar.gz"),
+    to = "/app.tar.gz"
+  )
+  x$RUN("remotes::install_local('/app.tar.gz')")
 
   x
 }

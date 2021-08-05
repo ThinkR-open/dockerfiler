6fc0c9923d8fbf54bc18f859f71a568d8fb6bc6f
josiahparry <josiah.parry@gmail.com>
2021-08-05 16:33:56 -0400

    utilize RSPM for package binaries

--- R/dock_from_renv.R
+++ R/dock_from_renv.R
@@ -4,6 +4,7 @@
 #' @param distro One of "focal", "bionic", "xenial", "centos7", or "centos8". See available distributions at https://hub.docker.com/r/rstudio/r-base/.
 #' @param primary_repo Default "CRAN". Indicates which repository in the `renv.lock` file to install the renv package from.
 #' @param out_dir The directory where to write the `Dockerfile`. When `NULL` no file is written. `NULL` by default.
+#' @param use_rspm Utilize the Public RStudio Package Manager for Linux binaries. Default `TRUE`.
 #'
 #' @return A R6 object of class `Dockerfile`.
 #' @details
@@ -12,11 +13,27 @@
 #'
 #' The R version is taken from the `renv.lock` file. Packages are installed using `renv::restore()` which ensures that the proper package version and source is used when installed.
 #'
+#' By default, `dock_from_renv()` will utilize the [public RStudio Package Manager](https://packagemanager.rstudio.com/) (PPM) to provide linux binaries. This dramatically improves install time. This is done when `use_rspm = TRUE`.  To do so, a new file `renv.lock.dock` will be created so as to not overwrite your existing `renv.lock` file. This new `renv.lock.dock` file is used to build the Docker image with PPM binaries.
+#'
+#' \donttest{
+#' dock <- dock_from_renv("renv.lock", distro = "xenial", out_dir = getwd())
+#' dock
+#' }
 #' @export
 dock_from_renv <- function(lockfile = "renv.lock", distro = "xenial", primary_repo = "CRAN",
-                           out_dir = NULL) {
+                           out_dir = NULL,
+                           use_rspm = TRUE) {
 
   lock <- renv:::lockfile(lockfile)
+
+  # change to public package manager by default, if false use default repo
+  if (use_rspm) {
+    lock$repos(CRAN = glue::glue("https://packagemanager.rstudio.com/all/__linux__/{distro}/latest"))
+    # write to tempfile
+    lockfile <- paste0(lockfile, ".dock")
+    lock$write(lockfile)
+  }
+
   # start the dockerfile
   dock <- Dockerfile$new(gen_base_image(distro = distro, r_version = lock$version()))
 
@@ -28,12 +45,16 @@ dock_from_renv <- function(lockfile = "renv.lock", distro = "xenial", primary_re
                         focal = list(os = "ubuntu", os_release = "20.04")
   )
 
+
   pkgs <- names(lock$data()$Packages)
 
   pkg_os <- lapply(pkgs, FUN = function(x) c(list(package = x), distro_args))
-  pkg_sysreqs <- lapply(x, function(x) do.call(pak::pkg_system_requirements, x))
 
-  pkg_installs <- unique(unlist(y))
+  message(sprintf("Fetching system dependencies for %s package records.", length(pkgs)))
+
+  pkg_sysreqs <- lapply(pkg_os, function(x) do.call(pak::pkg_system_requirements, x))
+
+  pkg_installs <- unique(unlist(pkg_sysreqs))
 
   if (distro %in% c("xenial" , "bionic" , "focal")) {
     dock$RUN("apt-get update -y")

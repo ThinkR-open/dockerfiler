1eac57fc08a9124b6150948ffec2089c315c5d9a
colin <colin@thinkr.fr>
2022-04-29 11:23:51 +0200

    style(linter): applied linter on dock_from_renv

--- R/dock_from_renv.R
+++ R/dock_from_renv.R
@@ -1,9 +1,11 @@
 # ignoring opensuse for the time being
-available_distros <- c("xenial" , "bionic" , "focal" , "centos7" , "centos8")
-
-
-
-
+available_distros <- c(
+  "xenial",
+  "bionic",
+  "focal",
+  "centos7",
+  "centos8"
+)
 
 #' Create a Dockerfile from an `renv.lock` file
 #'
@@ -11,8 +13,11 @@ available_distros <- c("xenial" , "bionic" , "focal" , "centos7" , "centos8")
 #' @param FROM Docker image to start FROM Default is
 #'     FROM rocker/r-base
 #' @param AS The AS of the Dockerfile. Default it NULL.
-#' @param distro One of "focal", "bionic", "xenial", "centos7", or "centos8". See available distributions at https://hub.docker.com/r/rstudio/r-base/.
-#' @param sysreqs boolean. If `TRUE`, the Dockerfile will contain sysreq installation.
+#' @param distro One of "focal", "bionic", "xenial", "centos7",
+#'     or "centos8". See available distributions
+#'     at https://hub.docker.com/r/rstudio/r-base/.
+#' @param sysreqs boolean. If `TRUE`, the Dockerfile
+#'     will contain sysreq installation.
 #' @param expand boolean. If `TRUE` each system requirement will have its own `RUN` line.
 #' @param repos character. The URL(s) of the repositories to use for `options("repos")`.
 #' @param extra_sysreqs character vector. Extra debian system requirements.
@@ -21,9 +26,14 @@ available_distros <- c("xenial" , "bionic" , "focal" , "centos7" , "centos8")
 #' @return A R6 object of class `Dockerfile`.
 #' @details
 #'
-#' System requirements for packages are provided through RStudio Package Manager via the {pak} package. The install commands provided from pak are added as `RUN` directives within the `Dockerfile`.
+#' System requirements for packages are provided
+#' through RStudio Package Manager via the {pak}
+#' package. The install commands provided from pak
+#' are added as `RUN` directives within the `Dockerfile`.
 #'
-#' The R version is taken from the `renv.lock` file. Packages are installed using `renv::restore()` which ensures that the proper package version and source is used when installed.
+#' The R version is taken from the `renv.lock` file.
+#' Packages are installed using `renv::restore()` which ensures
+#' that the proper package version and source is used when installed.
 #'
 #' @importFrom attempt map_try_catch
 #' @importFrom glue glue
@@ -34,16 +44,16 @@ available_distros <- c("xenial" , "bionic" , "focal" , "centos7" , "centos8")
 #' dock$write("Dockerfile")
 #' }
 #' @export
-dock_from_renv <- function(lockfile = "renv.lock",
-                           distro = "focal",
-                           FROM = "rocker/r-base",
-                           AS = NULL,
-                           sysreqs = TRUE,
-                           repos = c(CRAN = "https://cran.rstudio.com/"),
-                           expand = FALSE,
-                           extra_sysreqs = NULL
+dock_from_renv <- function(
+  lockfile = "renv.lock",
+  distro = "focal",
+  FROM = "rocker/r-base",
+  AS = NULL,
+  sysreqs = TRUE,
+  repos = c(CRAN = "https://cran.rstudio.com/"),
+  expand = FALSE,
+  extra_sysreqs = NULL
 ) {
-
   distro <- match.arg(distro, available_distros)
 
   lock <- getFromNamespace("lockfile", "renv")(lockfile)
@@ -52,24 +62,42 @@ dock_from_renv <- function(lockfile = "renv.lock",
   lockfile <- basename(lockfile)
 
   # start the dockerfile
-  R_major_minor <- paste(strsplit(lock$data()$R$Version, "[.]")[[1]][1:2], collapse = ".")
+  R_major_minor <- paste(
+    strsplit(lock$data()$R$Version, "[.]")[[1]][1:2],
+    collapse = "."
+  )
   dock <- Dockerfile$new(
     FROM = gen_base_image(
       distro = distro,
       r_version = R_major_minor,
       FROM = FROM
     ),
-    AS = AS)
+    AS = AS
+  )
 
   distro_args <- switch(
     distro,
-    centos7 = list(os = "centos", os_release = "7"),
-    centos8 = list(os = "centos", os_release = "8"),
-    xenial = list(os = "ubuntu", os_release = "16.04"),
-    bionic = list(os = "ubuntu", os_release = "18.04"),
-    focal = list(os = "ubuntu", os_release = "20.04")
+    centos7 = list(
+      os = "centos",
+      os_release = "7"
+    ),
+    centos8 = list(
+      os = "centos",
+      os_release = "8"
+    ),
+    xenial = list(
+      os = "ubuntu",
+      os_release = "16.04"
+    ),
+    bionic = list(
+      os = "ubuntu",
+      os_release = "18.04"
+    ),
+    focal = list(
+      os = "ubuntu",
+      os_release = "20.04"
+    )
   )
-
 
   install_cmd <- switch(
     distro,
@@ -79,6 +107,7 @@ dock_from_renv <- function(lockfile = "renv.lock",
     bionic = "apt-get install -y",
     focal = "apt-get install -y"
   )
+
   update_cmd <- switch(
     distro,
     centos7 = "yum update -y",
@@ -87,6 +116,7 @@ dock_from_renv <- function(lockfile = "renv.lock",
     bionic = "apt-get update -y",
     focal = "apt-get update -y"
   )
+
   clean_cmd <- switch(
     distro,
     centos7 = "yum clean all && rm -rf /var/cache/yum",
@@ -106,12 +136,32 @@ dock_from_renv <- function(lockfile = "renv.lock",
       bullet = "info",
       bullet_col = "green"
     )
-    message(sprintf("Fetching system dependencies for %s package records.", length(pkgs)))
-    pkg_os <- lapply(pkgs, FUN = function(x) c(list(package = x), distro_args))
+
+    message(
+      sprintf(
+        "Fetching system dependencies for %s package records.",
+        length(pkgs)
+      )
+    )
+
+    pkg_os <- lapply(
+      pkgs,
+      FUN = function(x) {
+        c(
+          list(package = x),
+          distro_args
+        )
+      }
+    )
 
     pkg_sysreqs <- attempt::map_try_catch(
       pkg_os,
-      function(x) do.call(pak::pkg_system_requirements, x),
+      function(x) {
+        do.call(
+          pak::pkg_system_requirements,
+          x
+        )
+      },
       .e = ~ character(0)
     )
 
@@ -125,41 +175,50 @@ dock_from_renv <- function(lockfile = "renv.lock",
       )
     }
 
-
     cat_green_tick("Done") # TODO animated version ?
   } else {
     pkg_installs <- NULL
   }
 
-
   # extra_sysreqs
 
-
   if (length(extra_sysreqs) > 0) {
-    extra <- paste(install_cmd,
-                   extra_sysreqs)
+    extra <- paste(
+      install_cmd,
+      extra_sysreqs
+    )
     pkg_installs <- c(pkg_installs, extra)
   }
 
-
   # compact
-
   if (!expand) {
     # we compact sysreqs
 
-    compact <-
-      paste(gsub(pkg_installs, pattern = install_cmd, replacement = ""),
-            collapse = " ")
+    compact <- paste(
+      gsub(
+        pkg_installs,
+        pattern = install_cmd,
+        replacement = ""
+      ),
+      collapse = " "
+    )
     if (compact != "") {
-      pkg_installs <- paste(update_cmd, "&&", install_cmd, compact,"&&",clean_cmd)
+      pkg_installs <- paste(
+        update_cmd,
+        "&&",
+        install_cmd,
+        compact,
+        "&&",
+        clean_cmd
+      )
     }
-  } else{
+  } else {
     dock$RUN(update_cmd)
   }
 
   do.call(dock$RUN, list(pkg_installs))
 
-  if (expand){
+  if (expand) {
     dock$RUN(clean_cmd)
   }
 
@@ -185,10 +244,11 @@ dock_from_renv <- function(lockfile = "renv.lock",
 #' Creates the base image name from the provided distro name and the R version found in the `renv.lock` file.
 #'
 #' @keywords internal
-gen_base_image <- function(distro = "bionic",
-                           r_version = "4.0",
-                           FROM = "rstudio/r-base") {
-
+gen_base_image <- function(
+  distro = "bionic",
+  r_version = "4.0",
+  FROM = "rstudio/r-base"
+) {
   distro <- match.arg(distro, available_distros)
 
   if (FROM == "rstudio/r-base") {

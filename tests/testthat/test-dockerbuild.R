2325b6a3bd657c6af3399390efddb03b4da8ecd8
vincent <vincent@thinkr.fr>
2022-06-22 10:18:14 +0200

    test docker build

---
+++ tests/testthat/test-dockerbuild.R
@@ -0,0 +1,26 @@
+
+
+test_that("dockerfile is buildable", {
+
+  descdir <- tempfile(pattern = "desc")
+  dir.create(descdir)
+  file.copy("DESCRIPTION", descdir)
+  my_dock <- dockerfiler:::dock_from_desc(file.path(descdir, "DESCRIPTION"))
+  tpf <- "Dockerfile"
+  dirname(tpf)
+  cat(".RData
+.Rhistory
+.git
+.gitignore
+manifest.json
+rsconnect/
+.Rproj.user",file = ".dockerignore")
+  # file.edit(".dockerignore")
+  my_dock$write(tpf)
+
+  out1 <-system("docker run hello-world ")
+expect_equal(out1,0)
+ out <- system(sprintf("docker build . --file %s ",tpf),wait = TRUE)
+# file.edit(tpf)
+expect_equal(out,0)
+})

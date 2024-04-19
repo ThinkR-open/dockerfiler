ec7206bfeb41279eb9ea2bf03eb00edc77bb3549
vincent <vincent@thinkr.fr>
2024-04-19 18:33:22 +0200

    update test

--- tests/testthat/test-get_sysreqs.R
+++ tests/testthat/test-get_sysreqs.R
@@ -1,13 +1,5 @@
 test_that("get_sysreqs works", {
   skip_on_cran()
-  skip_if_not(
-    attr(
-      curlGetHeaders(
-        "https://sysreqs.r-hub.io/pkg/mongolite/linux-x86_64-debian-gcc"
-      ),
-      "status"
-    ) == 200
-  )
   res <- get_batch_sysreqs(
     c("mongolite"),
     quiet = TRUE
@@ -18,24 +10,5 @@ test_that("get_sysreqs works", {
   expect_true(
     inherits(res, "character")
   )
-  # Test without internet
-  withr::with_envvar(
-    c(
-      "http_proxy" = "http://proxy:port",
-      "http_proxy_user" = "username",
-      "https_proxy" = "username",
-      "https_proxy_user" = "username",
-      "ftp_proxy" = "username"
-    ),
-    res <- get_sysreqs(
-      packages = "mongolite",
-      quiet = TRUE,
-      batch_n = 30
-    )
-  )
-  expect_equal(
-    res,
-    ""
-    #c("libsasl2-dev", "libssl-dev")
-  )
+
 })

1b7fa9b4a1f45b6451c8a772c526fbe849f746e1
Vincent Guyader <vincent@thinkr.fr>
2024-06-17 12:40:25 +0000

    move R_USER_CACHE_DIR in setup file

--- tests/testthat/test-get_sysreqs.R
+++ tests/testthat/test-get_sysreqs.R
@@ -1,8 +1,5 @@
 test_that("get_sysreqs works", {
   skip_on_cran()
-  R_USER_CACHE_DIR<-tempfile()
-  dir.create(R_USER_CACHE_DIR)
-  Sys.setenv("R_USER_CACHE_DIR"=R_USER_CACHE_DIR)
   res <- get_sysreqs(
     c("mongolite"),
     quiet = TRUE

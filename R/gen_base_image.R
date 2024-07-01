5ef486d101246a3b788c3b3e41af40b6cb2dfd04
vincent guyader <vincent@thinkr.fr>
2024-07-01 14:03:32 +0200

    remove the distro parameter  (#73)
    * remove the distro parameter
    
    * add dependencies parameter
    
    * update doc

--- R/gen_base_image.R
+++ R/gen_base_image.R
@@ -7,15 +7,15 @@
 #' @keywords internal
 #' @noRd
 gen_base_image <- function(
-  distro = "bionic",
+  distro = NULL,
   r_version = "4.0",
   FROM = "rstudio/r-base"
 ) {
-  distro <- match.arg(distro, available_distros)
 
-  if (FROM == "rstudio/r-base") {
-    glue::glue("{FROM}:{r_version}-{distro}")
-  } else {
+if (!is.null(distro)){
+  warning("the `distro` parameter is not used anymore, only debian/ubuntu based images are supported")
+}
+
     glue::glue("{FROM}:{r_version}")
-  }
+
 }
